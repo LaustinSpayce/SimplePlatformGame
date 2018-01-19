e6c5a5e51423f008bc73a8cabf3f49b7f6127f7f
LaustinSpayce <stuart.myers@gmail.com>
2018-01-19 20:14:53 +0800

    Add 3rd person controller

---
+++ SimplePlatformGame/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+	public Camera mainCamera;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+	}
+}
